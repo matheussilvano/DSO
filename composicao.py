33ab21dea06f710bfe2415e5b24586ab64a70a26
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2025-03-24 21:23:55 -0300

    Update composicao.py

--- composicao.py
+++ composicao.py
@@ -31,15 +31,75 @@ class Endereco:
 class Aluno:
 
     def __init__(self, nome: str, endereco: Endereco):
-        self.nome = nome
-        self.endereco = endereco
+        self.__nome = nome
+        self.__endereco = endereco
+
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome
+
+    @nome.getter
+    def nome(self):
+        return self.__nome
+    
+    @property
+    def endereco(self):
+        return self.__endereco
+    
+    @endereco.setter
+    def endereco(self, novo_endereco):
+        self.__endereco = novo_endereco
+
+    @endereco.getter
+    def endereco(self):
+        return self.__endereco
 
 class Professor:
 
     def __init__(self, nome: str, orientando: Aluno, endereco: Endereco):
-        self.nome = nome
-        self.orientando = orientando
-        self.endereco = endereco
+        self.__nome = nome
+        self.__orientando = orientando
+        self.__endereco = endereco
+
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome
+
+    @nome.getter
+    def nome(self):
+        return self.__nome
+
+    @property
+    def orientando(self):
+        return self.__orientando
+
+    @orientando.setter
+    def orientando(self, novo_orientando):
+        self.__orientando = novo_orientando
+
+    @orientando.getter
+    def orientando(self):
+        return self.__orientando
+    
+    @property
+    def endereco(self):
+        return self.__endereco
+    
+    @endereco.setter
+    def endereco(self, novo_endereco):
+        self.__endereco = novo_endereco
+
+    @endereco.getter
+    def endereco(self):
+        return self.__endereco
 
 endereco1 = Endereco('Floripa', 'SC')
 aluno1 = Aluno('Handryos', endereco1)
